64686e8be2ac4f67e79a2db5e090e6daf9f6833f
redperiabras <redperiabras@gmail.com>
2018-01-01 02:02:47 +0800

    Model Initializer

--- models/en_fl/__init__.py
+++ models/en_fl/__init__.py
@@ -3,9 +3,12 @@ import pickle
 
 from modules.model import NMT
 
-with open(os.getcwd()+'/models/en_fl/en-fl.nlm', 'rb') as f:
-	config = pickle.load(f)
-	model = NMT('fl-en', config)
-	model.load(f)
+if (os.path.isfile(os.getcwd()+'/models/en_fl/en-fl.nlm.best')):
+	with open(os.getcwd()+'/models/en_fl/en-fl.nlm.best', 'rb') as f:
+		config = pickle.load(f)
+		model = NMT('fl-en', config)
+		model.load(f)
 
-	f.close()
+		f.close()
+else:
+	model = None
--- models/fl_en/__init__.py
+++ models/fl_en/__init__.py
@@ -3,9 +3,12 @@ import pickle
 
 from modules.model import NMT
 
-with open(os.getcwd()+'/models/fl_en/fl-en.nlm.best', 'rb') as f:
-	config = pickle.load(f)
-	model = NMT('fl-en', config)
-	model.load(f)
+if (os.path.isfile(os.getcwd()+'/models/fl_en/fl-en.nlm.best')):
+	with open(os.getcwd()+'/models/fl_en/fl-en.nlm.best', 'rb') as f:
+		config = pickle.load(f)
+		model = NMT('fl-en', config)
+		model.load(f)
 
-	f.close()
+		f.close()
+else:
+	model = None

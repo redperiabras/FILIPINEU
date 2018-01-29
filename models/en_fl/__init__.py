07fff6d0eab28c4d821faac6bcf50327f746b2b3
Red Periabras <redperiabras@gmail.com>
2018-01-29 14:44:40 +0800

    Update

--- models/en_fl/__init__.py
+++ models/en_fl/__init__.py
@@ -3,8 +3,8 @@ import pickle
 
 from modules.model import NMT
 
-if (os.path.isfile(os.getcwd()+'/models/en_fl/en-fl.nlm.best')):
-	with open(os.getcwd()+'/models/en_fl/en-fl.nlm.best', 'rb') as f:
+if (os.path.isfile(os.getcwd()+'/models/en_fl/en-fl.nlm')):
+	with open(os.getcwd()+'/models/en_fl/en-fl.nlm', 'rb') as f:
 		config = pickle.load(f)
 		model = NMT('en-fl', config)
 		model.load(f)
--- models/fl_en/__init__.py
+++ models/fl_en/__init__.py
@@ -3,8 +3,8 @@ import pickle
 
 from modules.model import NMT
 
-if (os.path.isfile(os.getcwd()+'/models/fl_en/fl-en.nlm.best')):
-	with open(os.getcwd()+'/models/fl_en/fl-en.nlm.best', 'rb') as f:
+if (os.path.isfile(os.getcwd()+'/models/fl_en/fl-en.nlm')):
+	with open(os.getcwd()+'/models/fl_en/fl-en.nlm', 'rb') as f:
 		config = pickle.load(f)
 		model = NMT('fl-en', config)
 		model.load(f)
